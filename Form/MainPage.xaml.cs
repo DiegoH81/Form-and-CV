01cc5af639854bb38d6cd911bb78e00297a298b6
Diego Alejandro Hidalgo Machaca <135664425+DiegoH81@users.noreply.github.com>
2024-06-25 17:53:12 -0500

    Add files via upload

---
+++ Form/MainPage.xaml.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Form
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+        private void OnEnviarClicked(object sender, EventArgs e)
+        {
+            string nombre = NombreEntry.Text;
+            string apellido = ApellidoEntry.Text;
+            string direccion = DireccionEntry.Text;
+            string correo = CorreoEntry.Text;
+            string telefono = TelefonoEntry.Text;
+            DateTime fechaNacimiento = FechaNacimientoPicker.Date;
+            string genero = GeneroPicker.SelectedItem as string;
+            string idioma = IdiomaPicker.SelectedItem as string;
+
+            if (string.IsNullOrWhiteSpace(nombre) ||
+                string.IsNullOrWhiteSpace(apellido) ||
+                string.IsNullOrWhiteSpace(direccion) ||
+                string.IsNullOrWhiteSpace(telefono) ||
+                string.IsNullOrWhiteSpace(genero) ||
+                string.IsNullOrWhiteSpace(idioma))
+            {
+                string mensaje = "Todos los campos son obligatorios!";
+                DisplayAlert("AVISO", mensaje, "OK");
+            }
+            else
+            {
+                string mensaje = $"Nombre: {nombre}\n" +
+                    $"Apellido: {apellido}\n" +
+                    $"Dirección: {direccion}\n" +
+                    $"Correo Electrónico: {correo}\n" +
+                    $"Teléfono: {telefono}\n" +
+                    $"Fecha de Nacimiento: {fechaNacimiento:dd/MM/yyyy}\n" +
+                    $"Género: {genero}\n" +
+                    $"Idioma: {idioma}";
+                DisplayAlert("DATOS", mensaje, "OK");
+            }
+
+        }
+    }
+}
